918ad041daabe91478f69d3ac7bca76d98ae91e3
abhinavpb-249 <abhinavpb249@gmail.com>
2023-07-24 10:06:30 +0530

    update

--- doctors/views.py
+++ doctors/views.py
@@ -2,44 +2,61 @@ from django.shortcuts import render,redirect
 from patients.models import Doctors
 from patients.models import Patients
 
-# Create your views here.
-
-
 
 
 
 def doctors(request):
 
+    # Important! patient_diplay should initially be hidden and should only be viewable after
+    # entering the correct password
     login_display = "block"
     patient_display = "none"
 
-    doctor_objects = Doctors.objects.all()
+    #These values should be defined here to prevent 'referenced before assignment error' because
+    #these values are called but is not assigned before the submit using request.POST.
+    doctor_qs = None
+    patient_list = None
+    
 
-    doctor_patients= []
+    # doctor_objects = Doctors.objects.all()
 
-    for doctor_object in doctor_objects:
+    # doctor_patients= []
+
+    # for doctor_object in doctor_objects:
         
-        #get the queryset of all patients under a doctor
-        patients_qs = doctor_object.patients_set.all()
-        patient_list = list(patients_qs)
-        doctor_patients.append(patient_list)
+    #     #get the queryset of all patients under a doctor
+    #     patients_qs = doctor_object.patients_set.all()
+    #     patient_list = list(patients_qs)
+    #     doctor_patients.append(patient_list)
 
 
     
-    doctor = Doctors.objects.all().values()
+    #doctor = Doctors.objects.all().values()
+    #to get the list of doctors. not used in the below code.
     
-    master_list = zip(doctor_objects, doctor_patients) 
+    #master_list = zip(doctor_objects, doctor_patients) 
 
     if request.method=='POST':
-        username = request.POST['username']
-        if not len(username)==0:
+        got_username = request.POST['username']
+        got_password = request.POST['password']
+        doctor_qs = Doctors.objects.get(username=got_username)
+        #real_username = doctor_qs.username
+        real_password = doctor_qs.password
+
+        #patient_list = doctor_qs.patients_set.all()
+        patients_qs = doctor_qs.patients_set.all()
+        patient_list = list(patients_qs)
+        
+
+        if got_password == real_password:
             patient_display = "block"
             login_display = "none"
 
     context = {
-        'doctor_objects': doctor_objects,
-        'doctor_patients': doctor_patients,
-        'master_list':master_list,
+        'doctor_qs':doctor_qs,
+        # 'doctor_objects': doctor_objects,
+        'patient_list': patient_list,
+        # 'master_list':master_list,
         'patient_display':patient_display,
         'login_display':login_display,
     }
--- patients/models.py
+++ patients/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Doctors(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  username = models.CharField(max_length=50,default='username')
+  password = models.CharField(max_length=50,default='password')
 
 class Patients(models.Model):
     name = models.CharField(max_length=100)
