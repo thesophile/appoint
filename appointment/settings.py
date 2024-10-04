40a07c2249d59c343ddb1a525edb5fcbbdacb64d
thesophile <abhinavpb249@gmail.com>
2024-10-04 19:46:28 +0530

    test

--- appointment/settings.py
+++ appointment/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'appointment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
